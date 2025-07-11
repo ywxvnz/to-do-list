baf34338ae785f590a5e1f783fe256702a5792d4
ywxvnz <baaaneeesaaa@gmail.com>
2025-07-11 17:36:31 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,116 @@
+<?php 
+require 'db_conn.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>To-Do List</title>
+    <link rel="stylesheet" href="css/style1.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
+</head>
+<body>
+<div class="logo">
+    <img src="img/check.png" width="7%" style="display: block; margin: auto; padding: 10px;"/>
+</div>
+    <div class="main-section">
+       <div class="add-section">
+        <h1 style="text-align: center; font-family: Montserrat ; color: #FFFFFF; margin-top: 20px; margin-bottom: 20px"><Table>TO-DO LIST</Table></h1>
+          <form action="app/add.php" method="POST" autocomplete="off">
+             <?php if(isset($_GET['mess']) && $_GET['mess'] == 'error'){ ?>
+                <input type="text" 
+                     name="title" 
+                     style="border-color: #ff6666"
+                     placeholder="This field is required." />
+              <button type="Submit"><span>&#43;</span> ADD&nbsp;</button>
+
+             <?php }else{ ?>
+              <input type="text" 
+                     name="title" 
+                     placeholder="What do you need to do?" />
+              <button type="Submit"><span>&#43;</span> ADD&nbsp;</button>
+             <?php } ?>
+          </form>
+       </div>
+       <?php 
+          $todos = $conn->query("SELECT * FROM todos ORDER BY id DESC");
+       ?>
+       <div class="show-todo-section">
+            <?php if($todos->rowCount() <= 0){ ?>
+                <div class="todo-item">
+                    <div class="empty">
+                        <img src="img/addsome.gif" width="300px">
+                    </div>
+                </div>
+            <?php } ?>
+
+            <?php while($todo = $todos->fetch(PDO::FETCH_ASSOC)) { ?>
+                <div class="todo-item">
+                    <span id="<?php echo $todo['id']; ?>"
+                          class="remove-to-do">x </span>
+                    <?php if($todo['checked']){ ?> 
+                        <input type="checkbox"
+                               class="check-box"
+                               data-todo-id ="<?php echo $todo['id']; ?>"
+                               checked />
+                        <h2 class="checked"><?php echo $todo['title'] ?></h2>
+                    <?php }else { ?>
+                        <input type="checkbox"
+                               data-todo-id ="<?php echo $todo['id']; ?>"
+                               class="check-box" />
+                        <h2><?php echo $todo['title'] ?></h2>
+                    <?php } ?>
+                    <br>
+                    <small>created: <?php echo $todo['date_time'] ?></small> 
+                </div>
+            <?php } ?>
+       </div>
+    </div>
+
+    <script src="js/jquery-3.7.1.min.js"></script>
+
+    <script>
+        $(document).ready(function(){
+            $('.remove-to-do').click(function(){
+                const id = $(this).attr('id');
+
+                $.post("app/remove.php", 
+                      {
+                          id: id
+                      },
+                      (data)  => {
+                         if(data){
+                             $(this).parent().hide(600);
+                         }
+                      }
+                );
+            });
+
+            $(".check-box").click(function(e){
+                const id = $(this).attr('data-todo-id');
+
+                $.post('app/check.php', 
+                      {
+                          id: id
+                      },
+                      (data) => {
+                          if(data != 'error'){
+                              const h2 = $(this).next();
+                              if(data === '1'){
+                                  h2.removeClass('checked');
+                              }else {
+                                  h2.addClass('checked');
+                              }
+                          }
+                      }
+                );
+            });
+        });
+    </script>
+</body>
+</html>
+
